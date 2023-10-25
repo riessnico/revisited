e1c882542dc2d7f7e13eb712c68288d91924641c
Nicolau <nicolauriess@gmail.com>
2023-10-25 00:25:14 -0300

    more design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,29 @@
-import { type AppType } from "next/app";
+import { type AppType } from 'next/app';
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api';
 
-import "~/styles/globals.css";
+import '~/styles/globals.css';
 
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from 'next-themes';
+import Layout from './layout';
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<main className={inter.className}>
-			<Component {...pageProps} />
-		</main>
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="dark"
+			disableTransitionOnChange
+		>
+			<main className={inter.className}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</main>
+		</ThemeProvider>
 	);
 };
 
